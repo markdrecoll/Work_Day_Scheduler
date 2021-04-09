a96ecb7606ae8349e3bff50ce3a85cac187935a5
mdre797 <markdrecoll@gmail.com>
2021-04-08 19:18:47 -0500

    Fixed button extracting data from text input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,9 @@ var time = moment().format('dddd MMMM YYYY');
 $("#currentDay").text(time);
 
 
-var nineAM = $('#9amInput').val();
+// var nineAM = $('#9amInput').val();
 
-$('#9amButton').click(function(){
-    alert("9am button was clicked" + nineAM);
+$("#9amButton").on("click", function(){
+    var nineAM = $("#9amInput").val();
+    alert(nineAM);
 });
