f47450b04223cfceb93cf6a1af3ad870d87bbfbb
mdre797 <markdrecoll@gmail.com>
2021-04-09 17:14:01 -0500

    Fixed local storage. Program Fully Functional.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
+// pull the current hour in 24 hour format and establish current day
 var currentHour = moment().format('HH');
-$('#currentDay').text(currentHour);
-
-var time = moment().format('dddd MMMM YYYY');
-$("#currentDay").text(time);
+var currentDateTime = moment().format('dddd MMMM YYYY');
+$("#currentDay").text(currentDateTime);
 
 // $("#9button").on("click", function(){
 //     var nineAM = $("#9").val();
@@ -14,24 +13,31 @@ for(var i = 9; i < 17; i++){
 
     var currentTextBox = $('#' + i);
 
-    var currentUserInputedText = 'userInputedText' + i;
+    // var currentUserInputedText = 'userInputedText' + i;
 
-    currentButton.on('click', function(){
-        var userAgendaItem = currentTextBox.val();
-        localStorage.setItem(currentUserInputedText, userAgendaItem);
-    });
+    // currentButton.on('click', function(){
+    //     var userAgendaItem = currentTextBox.val();
+    //     localStorage.setItem(currentUserInputedText, userAgendaItem);
+    // });
 };
 
-$(document).ready(function (){
-    console.log("page loaded");
-    var nineAmToDo = localStorage.getItem('nineAmToDos');
 
-    $("#10").val("test");
+$('#timeBlockContainer').on('click', '.buttonToBeSaved', function(e){
+    var currentID = ($(e.target).siblings().next().attr('id'));
+    localStorage.setItem(currentID, $(`#${currentID}`).val());
+});
+
+// on page load: color the text boxes
+$(document).ready(function (){
 
     for(var i=9; i<=17; i++){
 
         // create a variable that concatenates to get the html element
-        var currentRef = $('#' + i);
+        // var currentRef = $('#' + i);
+        var currentRef = $(`#${i}`);
+
+        console.log(currentRef);
+        $(`#${i}`).val(localStorage.getItem(i));
 
         // use an if statement to compare textbox's time value against real world time
         if(currentHour > i){
