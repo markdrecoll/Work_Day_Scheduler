7cf1c8870d0ea8f74d09c64cce6f84344a39faef
mdre797 <markdrecoll@gmail.com>
2021-04-09 16:13:29 -0500

    Local storage working. Applied colors for hours based on real time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,33 @@ $('#currentDay').text(currentHour);
 var time = moment().format('dddd MMMM YYYY');
 $("#currentDay").text(time);
 
+$("#9button").on("click", function(){
+    var nineAM = $("#9").val();
 
-// var nineAM = $('#9amInput').val();
+   //get the reference to the 9am text box here
+   localStorage.setItem('nineAmToDos', nineAM);
 
-$("#9amButton").on("click", function(){
-    var nineAM = $("#9amInput").val();
-    alert(nineAM);
+});
+
+
+$(document).ready(function (){
+    console.log("page loaded");
+    var nineAmToDo = localStorage.getItem('nineAmToDos');
+    $("#9").val(nineAmToDo);
+    $("#10").val("test");
+
+    for(var i=9; i<=19; i++){
+
+        // create a variable that concatenates to get the html element
+        var currentRef = $('#' + i);
+
+        // use an if statement to compare textbox's time value against real world time
+        if(currentHour > i){
+            $(currentRef).addClass('past');
+        }else if(currentHour == i){
+            $(currentRef).addClass('present');
+        }else{
+            $(currentRef).addClass('future');
+        }
+    }
 });
